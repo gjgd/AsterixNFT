e7541c53b76041acfad45709be9fa039b6419869
gjgd <gjgd@protonmail.com>
2021-10-24 15:01:15 -0500

    chore: display message if user is not on rinkeby

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,6 +29,14 @@ const App = () => {
           return;
       } else {
           console.log("We have the ethereum object", ethereum);
+          let chainId = await ethereum.request({ method: 'eth_chainId' });
+          console.log("Connected to chain " + chainId);
+
+          // String, hex code of the chainId of the Rinkebey test network
+          const rinkebyChainId = "0x4";
+          if (chainId !== rinkebyChainId) {
+            alert("You are not connected to the Rinkeby Test Network!");
+          }
       }
 
       /*
